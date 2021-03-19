fa136d2f1ec2e38ed54c2ff4a13e88d41422846b
dev-acastro <70414513+dev-acastro@users.noreply.github.com>
2021-03-19 00:04:46 -0600

    Third

--- patients/index.js
+++ patients/index.js
@@ -2,15 +2,23 @@ $(document).ready(function(){
 
 
 
-$('form').submit(function (e){
+$("form").submit(function (e){
 
-   console.log($("form").serializeArray());
+    var UserData = $("form").serialize();
+
+
+  // var s = JSON.stringify(UserData);
+
+
+
+  // var firstName = $("#firstName").val();
+   console.log(UserData);
     e.preventDefault();
 
-
-     $.each(x, function(i, field){
-         $("#results").append(field.name + ":" + field.value + " ");
-     });
+     // $.each(UserData, function(i, field){
+     //     $("#results").append(field.firstName + ":" + field.lastName + " ");
+     //     console.log(i + "" + field);
+     // });
 });
 
 //form
