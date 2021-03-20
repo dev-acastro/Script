8428cf077b92e26eac88d793d571bc297afdef42
dev-acastro <70414513+dev-acastro@users.noreply.github.com>
2021-03-19 18:59:59 -0600

    CORS ISSUE

--- patients/index.js
+++ patients/index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 $("form").submit(function (e){
 
-    var UserData = $("form").serialize();
+    var UserData = $("form").serializeArray();
 
 
   // var s = JSON.stringify(UserData);
@@ -15,11 +15,77 @@ $("form").submit(function (e){
    console.log(UserData);
     e.preventDefault();
 
-     // $.each(UserData, function(i, field){
-     //     $("#results").append(field.firstName + ":" + field.lastName + " ");
-     //     console.log(i + "" + field);
-     // });
+    var data = [];
+
+     $.each(UserData, function(i, field){
+         data[field.name] = field.value;
+     });
+
+     var jsonData = JSON.stringify(data);
+
+     console.log(jsonData);
+
+     postPatient(jsonData);
+
+
+
+
 });
+
+function postPatient(data) {
+
+
+
+    $.ajax({
+        type: 'POST',
+        url: "https://prod.hs1api.com/oauth/client_credential/accesstoken?grant_type=client_credentials",
+        data: {
+            "client_id": "kdqoHWRZ1YB5M99QVrtc9p4v2kxSct89",
+            "client_secret" : "IqHYGkvrsN7eFfRe"
+        },
+        beforeSend: function (request) {
+            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+        },
+        success: function (response) {
+
+
+            var access_token = response.access_token ;
+            $.ajax({
+                method: 'POST',
+                url: "https://prod.hs1api.com/ascend-gateway/api/v1/patients",
+                headers: {
+                    "Organization-ID": "5e7b7774c9e1470c0d716320",
+                    "Authorization": "Bearer BMui7AvkRGC9OZQ8joJG2G8CaSTI",
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin:" : "*"
+                },
+                crossDomain: true,
+                xhrFields: {
+                    withCredentials: true
+                },
+                data: JSON.stringify({
+                    "firstName": "Arturo",
+                    "lastName": "Castro",
+                    "dateOfBirth": "03/12/1991",
+                    "address1": "Av. Jerusalem 4152 ",
+                    "city": "SS",
+                    "state": "CA",
+                    "postalCode": "0000"
+                }),
+                success: function (response) {
+                    console.log(response)
+                },
+                error: function (error){
+                    console.log(error);
+                }
+
+            });
+        }
+
+
+    });
+}
 
 //form
 
