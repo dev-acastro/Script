910503fe2356043bfcc62f8b7806da60d04fc4f9
dev-acastro <70414513+dev-acastro@users.noreply.github.com>
2021-03-21 00:52:28 -0600

    fetching

--- patients/index.js
+++ patients/index.js
@@ -3,17 +3,9 @@ $(document).ready(function(){
 
 
 $("form").submit(function (e){
+    e.preventDefault();
 
     var UserData = $("form").serializeArray();
-
-
-  // var s = JSON.stringify(UserData);
-
-
-
-  // var firstName = $("#firstName").val();
-   console.log(UserData);
-    e.preventDefault();
 
     var data = [];
 
@@ -21,62 +13,63 @@ $("form").submit(function (e){
          data[field.name] = field.value;
      });
 
-     var jsonData = JSON.stringify(data);
 
-
-
-     postPatient(jsonData);
-
-
+     postPatient( JSON.stringify(data), auth());
 
 
 });
 
-function postPatient(data) {
+    function auth (){
+        $.ajax({
+            type: 'POST',
+            url: "https://prod.hs1api.com/oauth/client_credential/accesstoken?grant_type=client_credentials",
+            data: {
+                "client_id": "kdqoHWRZ1YB5M99QVrtc9p4v2kxSct89",
+                "client_secret" : "IqHYGkvrsN7eFfRe"
+            },
+
+            beforeSend: function (request) {
+                request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+            },
+            success: function (response) {
+                console.log(response.access_token);
+                return response.access_token;
+
+            }
+
+
+        });
+    }
 
 
 
-    $.ajax({
-        type: 'POST',
-        url: "https://prod.hs1api.com/oauth/client_credential/accesstoken?grant_type=client_credentials",
-        data: {
-            "client_id": "kdqoHWRZ1YB5M99QVrtc9p4v2kxSct89",
-            "client_secret" : "IqHYGkvrsN7eFfRe"
-        },
-        beforeSend: function (request) {
-            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-        },
-        success: function (response) {
+function postPatient(data, token) {
 
-            var access_token = response.access_token ;
             $.ajax({
                 method: 'POST',
                 url: "https://prod.hs1api.com/ascend-gateway/api/v1/patients",
-                headers: {
-                    "Organization-ID": "5e7b7774c9e1470c0d716320",
-                    "Authorization": "Bearer BMui7AvkRGC9OZQ8joJG2G8CaSTI",
-                    // "Accept": "application/json",
-                    // "Content-Type": "application/json",
-                    //"Access-Control-Allow-Origin" : "*",
-                   "Access-Control-Allow-Headers": "x-requested-with"
-                },
-                // crossDomain: true,
-                // xhrFields: {
-                //     withCredentials: true
+                // headers: {
+                //     "Organization-ID": "5e7b7774c9e1470c0d716320",
+                //     "Authorization": "Bearer "+token,
                 // },
+
+                beforeSend: function (request) {
+                    request.setRequestHeader("Organization-ID", "5e7b7774c9e1470c0d716320")
+                    request.setRequestHeader("Authorization", "Bearer "+token)
+                    request.setRequestHeader('Access-Control-Allow-Origin', "*")
+                },
+
                 data: data,
                 success: function (response) {
                     console.log(response)
                 },
                 error: function (e){
-                    //console.log(e);
+                    console.log(e);
                 }
 
+
             });
-        }
 
-
-    });
 }
 
 //form
