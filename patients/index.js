fc7a2e6412abd683d1bd69f6ce774564fafb094d
dev-acastro <70414513+dev-acastro@users.noreply.github.com>
2021-03-29 00:32:39 -0600

    dataForm

--- patients/index.js
+++ patients/index.js
@@ -1,72 +1,63 @@
-"use strict"
+
 
 $(document).ready(function(){
 
-    var dataExample = {
-        firstName: "Arturo",
-        lastName: "Miguel",
-        address1: "Av. Masferrer 163 B Col. Campestre",
-        city: "San Salvador",
-        state: "CA",
-        postalCode: "0000",
-        gender: "M",
-        dateOfBirth: "2021-03-03"
+    auth();
+
+
+    function getDataForm(e){
+        e.preventDefault();
+
+        var userData = $(this).serializeArray();
+
+        var data = [];
+
+        $.each(UserData, function(i, field){
+            data[field.name] = field.value;
+        });
+
+        console.log(userData);
     }
 
-    postPatient(JSON.stringify(dataExample));
+    function auth (){
 
-/*$("form").submit(function (e){
-    e.preventDefault();
-
-    var UserData = $("form").serializeArray();
-
-    var data = [];
-
-     $.each(UserData, function(i, field){
-         data[field.name] = field.value;
-     });
-
-    var dataExample = {
-        firstName: "Arturo",
-        lastName: "Miguel",
-        address: "Av. Masferrer 163 B Col. Campestre",
-        city: "San Salvador",
-        state: "CA",
-        postalCode: "0000",
-        gender: "M",
-        dateOfBirth: "2021-03-03"
-    }
-     postPatient(JSON.stringify(dataExample), auth());
-});*/
-
-    /*function auth (){
-
-        var token ="";
+      //  var token ="";
 
          $.ajax({
-            type: 'POST',
-            url: "https://prod.hs1api.com/oauth/client_credential/accesstoken?grant_type=client_credentials",
-            data: {
-                "client_id": "kdqoHWRZ1YB5M99QVrtc9p4v2kxSct89",
-                "client_secret": "IqHYGkvrsN7eFfRe"
-            },
-            beforeSend: function (request) {
-                request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-            },
+            type: 'GET',
+            url: "http://icjmedia.org/api/",
             success: function (response) {
-                token = response.access_token;
+                console.log(response)
             }
         });
 
-         console.log(token)
-        return token;
-    }*/
+
+    }
+
+function getPatients(){
+    $.ajax({
+        method: 'GET',
+        url: "https://prod.hs1api.com/ascend-gateway/api/v1/patients",
+        crossDomain: true,
+        headers: {
+            "Authorization" : "Bearer EInjlHiva9gYTmmcPU34vaC3f6YF" ,
+            "Organization-Id" : "5e7b7774c9e1470c0d716320",
+        },        
+        dataType: "json",
+        success: function (response) {
+
+        },
+        error: function (e){
+
+        }
+
+
+    });
+}
 
 
 
 function postPatient(data) {
-
-            var token = auth();
 
 
             $.ajax({
@@ -74,13 +65,13 @@ function postPatient(data) {
                 url: "https://prod.hs1api.com/ascend-gateway/api/v1/patients",
                 crossDomain: true,
                 headers: {
-                    "Authorization" : "Bearer " + token,
+                    "Authorization" : "Bearer EInjlHiva9gYTmmcPU34vaC3f6YF" ,
                     "Organization-Id" : "5e7b7774c9e1470c0d716320",
                 },
                 data: data,
                 dataType: "json",
                 success: function (response) {
-
+                    console.log(response);
                 },
                 error: function (e){
 
